b06797749f8cf296f57e5a73b14fbf5601703a50
VadimVJ <VadimVJ12021965@mail.ru>
2024-07-06 21:20:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ scren_hight = 600
 screen = paygame.displey.set_mode((screen_widht,scren_hight))
 
 paygame.displey.set_caption("ИГРА ТИР")
-icon = paygame.image.load("img/1642365819_1-papik-pro-p-tir-klipart-1.png")
+icon = paygame.image.load("img/archer.jpg")
 paygame.displey.set_icon(icon)
 
 target_img = paygame.image.load("img/target.png")
