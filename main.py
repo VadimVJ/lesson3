0b5f64c0a8420f6982cc529a405c8c098c1f03d3
VadimVJ <VadimVJ12021965@mail.ru>
2024-07-06 19:29:21 +0500

    add-new-const

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import paygame
 
 paygame.init()
 
-running = true
+screen_widht = 800
+scren_hight = 600
+screen = paygame.displey.set_mode((screen_widht,scren_hight))
+
+paygame.displey.set_caption("ИГРА ТИР")
+icon = paygame.image.load("")
+
+running = True
 while running:
     pass
 
