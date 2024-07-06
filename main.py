e512f2a0fd1a8a8cec839507008f38602a85ba57
VadimVJ <VadimVJ12021965@mail.ru>
2024-07-06 23:35:12 +0500

    add all cycle code

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
-import paygame
+import pygame
 import random
-paygame.init()
+pygame.init()
 
 screen_widht = 800
 scren_hight = 600
-screen = paygame.displey.set_mode((screen_widht,scren_hight))
+screen = pygame.display.set_mode((screen_widht,scren_hight))
 
-paygame.displey.set_caption("ИГРА ТИР")
-<<<<<<< Updated upstream
-icon = paygame.image.load("img/archer.jpg")
-=======
-icon = paygame.image.load("img/archer.jpg")
->>>>>>> Stashed changes
-paygame.displey.set_icon(icon)
+pygame.display.set_caption("ИГРА ТИР")
+icon = pygame.image.load("img/archer.jpg")
+icon = pygame.image.load("img/archer.jpg")
+pygame.display.set_icon(icon)
 
-target_img = paygame.image.load("img/target.png")
+target_img = pygame.image.load("img/target.png")
 target_widht=80
 target_hight=80
 
-target_x = random.randint(0, screen_wight - target_wight)
-target_y = random.randint(0, screen_hight - target_hight)
+target_x = random.randint(0, screen_widht - target_widht)
+target_y = random.randint(0, screen_widht - target_hight)
 
-color(random.randint(0,255), random.randint( 0,255), random.randint(0,255)
+color = (random.randint(0,255), random.randint( 0,255), random.randint(0,255))
 
 
 running = True
 while running:
     pass
+    screen.fill(color)
+    for event in pygame.event.get():
+     if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.mousebuttondown:
+            mouse_x, mouse_y = pygame.get_pos()
+            if target_x < mouse_x < target_x + target_widht and target_y < mouse_y < target_y + target_hight:
+                target_x = random.randint(0, SCREEN_WIGHT - target_wight)
+                target_y = random.randint(0, SCREEN_HIGHT - target_hight)
 
-paygame.quit()
+    screen.blit(target_img,(target_x,target_y))
+    pygame.display.update()
+
+pygame.quit()
