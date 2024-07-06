044e5733c1968a29f6a56552483a76bcfb3301fc
VadimVJ <VadimVJ12021965@mail.ru>
2024-07-06 21:43:19 +0500

    add al const

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ scren_hight = 600
 screen = paygame.displey.set_mode((screen_widht,scren_hight))
 
 paygame.displey.set_caption("ИГРА ТИР")
+<<<<<<< Updated upstream
 icon = paygame.image.load("img/archer.jpg")
+=======
+icon = paygame.image.load("img/strelok.jpg")
+>>>>>>> Stashed changes
 paygame.displey.set_icon(icon)
 
 target_img = paygame.image.load("img/target.png")
