c1d7ec51342dba0309017cb2024744042f55b7e0
VadimVJ <VadimVJ12021965@mail.ru>
2024-07-06 20:35:03 +0500

    add all const

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import paygame
-
+import random
 paygame.init()
 
 screen_widht = 800
@@ -7,7 +7,18 @@ scren_hight = 600
 screen = paygame.displey.set_mode((screen_widht,scren_hight))
 
 paygame.displey.set_caption("ИГРА ТИР")
-icon = paygame.image.load("")
+icon = paygame.image.load("img/1642365819_1-papik-pro-p-tir-klipart-1.png")
+paygame.displey.set_icon(icon)
+
+target_img = paygame.image.load("img/target.png")
+target_widht=80
+target_hight=80
+
+target_x = random.randint(0, screen_wight - target_wight)
+target_y = random.randint(0, screen_hight - target_hight)
+
+color(random.randint(0,255), random.randint( 0,255), random.randint(0,255)
+
 
 running = True
 while running:
