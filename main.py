07250b7ed89429d774c5be7deb00db7e7dfee1c1
VadimVJ <VadimVJ12021965@mail.ru>
2024-07-08 14:52:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 import random
+
+from pygame import Surface, SurfaceType
+
 pygame.init()
 
 screen_widht = 800
@@ -9,7 +12,7 @@ screen = pygame.display.set_mode((screen_widht,scren_hight))
 pygame.display.set_caption("ИГРА ТИР")
 icon = pygame.image.load("img/archer.jpg")
 pygame.display.set_icon(icon)
-screen = pygame.display.set_mode((screen_widht,scren_hight))
+screen: Surface | SurfaceType = pygame.display.set_mode((screen_widht,scren_hight))
 
 pygame.display.set_caption("ИГРА ТИР")
 icon = pygame.image.load("img/archer.jpg")
@@ -20,29 +23,23 @@ target_widht=80
 target_hight=80
 
 target_x = random.randint(0, screen_widht - target_widht)
-target_y = random.randint(0, screen_widht - target_high)
+target_y = random.randint(0, screen_widht - target_hight)
 
 color = (random.randint(0,255), random.randint( 0,255), random.randint(0,255))
 
 
 running = True
 while running:
-    pass
-    screen.fill(color)
-    for event in pygame.event.get():
-     if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.mousebuttondown:
-            mouse_x, mouse_y = pygame.get_pos()
-    for event in paygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            mouse_x, mouse_y = pygame.get_pos()
-            if target_x < mouse_x < target_x + target_widht and target_y < mouse_y < target_y + target_hight:
-                target_x = random.randint(0, SCREEN_WIGHT - target_wight)
-                target_y = random.randint(0, SCREEN_HIGHT - target_hight)
+        screen.fill(color)
+         for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_x, mouse_y = pygame.get_pos()
+                         if target_x < mouse_x < target_x + target_widht and target_y < mouse_y < target_y + target_hight:
+                        target_x = random.randint(0, SCREEN_WIGHT - target_wight)
+                        target_y = random.randint(0, SCREEN_HIGHT - target_hight)
+         screen.blit(target_img,(target_x,target_y))
+         pygame.display.update()
 
-    screen.blit(target_img,(target_x,target_y))
-    pygame.display.update()
-
-pygame.quit()
+        pygame.quit()
